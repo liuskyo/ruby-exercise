c79dc9eded0d8b16d20ff4314b898cf3a200dbfd
劉天佑 <gn00595067@gmail.com>
2015-12-08 12:23:45 +0800

    class p2 ex2

---
+++ classes_and_objects_part2/ex2.rb
@@ -0,0 +1,48 @@
+class MyCar
+	attr_reader:year
+	attr_accessor:color
+	attr_accessor:model
+    attr_accessor:speed
+    attr_accessor:gas_mileage
+
+	
+
+
+	def initialize(year,color,model)
+		@year=year
+		@color=color
+		@model=model
+		@speed=0
+		@gas_mileage=0
+	end
+
+	def speed_up
+		@speed=@speed+10
+	end
+
+	def brake
+		if @speed>=10
+			@speed=@speed-10
+		else
+			@speed=0
+		end
+	end
+
+	def shut_off
+		@speed=0
+	end
+
+	def method_namspray_paint(color)
+		@color=color
+	end
+
+	def to_s
+		"MyCar is a #{@year}-#{@color}-#{@model}"
+	end
+
+end
+
+
+car1=MyCar.new(1988,"blue","Bezn")
+
+puts car1
