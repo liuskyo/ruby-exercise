f98c948bdeebce27b63093e116c2630f8b9d3a17
劉天佑 <gn00595067@gmail.com>
2015-12-08 12:22:26 +0800

    basic ex3

---
+++ basic/ex3.rb
@@ -0,0 +1,7 @@
+movies={"movieA"=>1975,"movieB"=>2004,"movieC"=>2013,"movieD"=>2001,"movieE"=>1981}
+
+puts movies["movieA"]
+puts movies["movieB"]
+puts movies["movieC"]
+puts movies["movieD"]
+puts movies["movieE"]
